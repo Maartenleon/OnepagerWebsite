bdc4a56fba30eb92ab749cafa3633031209a1cc8
Maartenleon <maarten.stienstra@gmail.com>
2018-02-14 14:15:49 +0100

    personal website

---
+++ javascript/javascript.js
@@ -0,0 +1,22 @@
+// sticky menu
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+// scroll to section
+$("nav").find("a").click(function(e) {
+    e.preventDefault();
+    var section = $(this).attr("href");
+    $("html, body").animate({
+        scrollTop: $(section).offset().top
+    });
+});
